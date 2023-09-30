52fc0cf07521bc5f5ce20e64a47dfeb57714e5e8
Kiarash Shojaei <71709479+kshyst@users.noreply.github.com>
2023-09-30 19:49:22 +0330

    Add files via upload

---
+++ CurrencyConvertor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CurrencyConvertor
+{
+    static class CurrencyConvertor
+    {
+        private static float DollarPrice = 50000;
+        public static float TomanToDollar(this float Money)
+        {
+            float temp = Money / DollarPrice;
+
+            return Math.Abs(temp);
+        }
+        public static float DollarToToman(this float Money)
+        {
+            float temp = Money * DollarPrice;
+
+            return temp;
+        }
+    }
+
+}
---
+++ CurrencyConvertorSession.cs
@@ -0,0 +1,46 @@
+﻿
+namespace CurrencyConvertor
+{
+    class Session
+    {
+        private int Mode = 3;
+
+        public Session() { }
+
+        public void StartSession()
+        {
+            while (Mode != 0)
+            {
+                Console.WriteLine("Welcome ! What Do You Want To Do???");
+                Console.WriteLine("[1] Convert Toman To Dollar");
+                Console.WriteLine("[2] Convert Dollar To Toman");
+                Console.WriteLine("[0] Exit");
+                Mode = Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+
+                switch (Mode)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        Console.Clear();
+                        break;
+                    case 1:
+                        Console.WriteLine("Enter Toman");
+                        float money1 = Convert.ToSingle(Console.ReadLine());
+                        Console.WriteLine("Your Money in Dollars = " + money1.TomanToDollar());
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Dollar");
+                        float money2 = Convert.ToSingle(Console.ReadLine());
+                        Console.WriteLine("Your Money in Tomans = " + money2.DollarToToman());
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+} 
